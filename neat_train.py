26bc9a8455f915eead2d96f01238bab4149bd7f4
SaiEthihasChanda <saiethihaschanda@gmail.com>
2024-04-21 22:27:30 +0530

    checkpoint 5

--- neat_train.py
+++ neat_train.py
@@ -21,16 +21,18 @@ class pacman_game:
         self.clock = pygame.time.Clock()
         self.turning_points = self.game.nodes.nodesLUT.keys()
         self.intersection_points = [[], []]
+        self.last_position = None  # Store the last position of the agent
+        self.last_position_time = None
         for node in self.turning_points:
             self.intersection_points[0].append(node[0])
             self.intersection_points[1].append(node[1])
 
             # (node.position.x, node.position.y)
-        print(self.intersection_points[0])
-        print(self.intersection_points[1])
+        # print(self.intersection_points[0])
+        # print(self.intersection_points[1])
 
-        print("pacman at")
-        print(self.pacman.position)
+    # print("pacman at")
+    # print(self.pacman.position)
 
     def test_ai(self):
         run = True
@@ -69,164 +71,105 @@ class pacman_game:
             pygame.display.update()
 
             # Check if Pac-Man's position matches any of the node positions
-            pacman_pos = (game_info.pacman_pos_x.x, game_info.pacman_pos_x.y)
+
             # print(pacman_pos)
-            current_position = (game_info.pacman_pos_x.x, game_info.pacman_pos_x.y)
-
-            if current_position != last_position:
-                last_position = current_position
-                current_time = datetime.datetime.now()
-                time_string = current_time.strftime("%H%M%S")
-                last_position_time = time_string
-                # print(last_position)
-                # last_position_time = time.time()
-            elif current_position == last_position:
-                current_time = datetime.datetime.now()
-                time_string = current_time.strftime("%H%M%S")
-                if int(time_string) - int(last_position_time) > 1.5:
-                    self.calculate_fitness(genome1, genome2, game_info)
-                    break
-
-            for node_pos in self.turning_points:
-                if pacman_pos == node_pos:
-                    print("intersection")
-                    # self.move_ai(net1,game_info)
-
-                    # print("intersection")
-                    current_time = datetime.datetime.now()
-                    time_string = current_time.strftime("%H%M%S")  # Format as string
-                    print("Current time:", time_string)
-                    output1 = net1.activate((game_info.pacman_pos_x.x, game_info.pacman_pos_x.y,
-                                             game_info.ghosts_pos[0].x - game_info.pacman_pos_x.x,
-                                             game_info.ghosts_pos[0].y - game_info.pacman_pos_x.y,
-                                             game_info.ghosts_pos[1].x - game_info.pacman_pos_x.x,
-                                             game_info.ghosts_pos[1].y - game_info.pacman_pos_x.y,
-                                             game_info.ghosts_pos[2].x - game_info.pacman_pos_x.x,
-                                             game_info.ghosts_pos[2].y - game_info.pacman_pos_x.y,
-                                             game_info.ghosts_pos[3].x - game_info.pacman_pos_x.x,
-                                             game_info.ghosts_pos[3].y - game_info.pacman_pos_x.y,
-                                             game_info.score
-                                             ))
-
-                    # print(game_info.ghosts_pos[0].x)
-                    # print(self.pacman.position.x)
-                    # print("output done")
-
-                    # Feed Pac-Man's position and other relevant data to the neural network
-
-                    # Determine the decision based on the neural network output
-                    decision = output1.index(max(output1))
-                    if decision == 0:
-                        # self.game.pacman.direction = UP
-                        keyboard.press('w')
-                        keyboard.release('a')
-                        keyboard.release('s')
-                        keyboard.release('d')
-                        # new_time = datetime.datetime.now()
-                        # new_time_string = new_time.strftime("%H%M%S")
-                        # time_stamp = new_time_string
-                        print("up")
-
-                    elif decision == 1:
-                        keyboard.press('s')
-                        keyboard.release('d')
-                        keyboard.release('a')
-                        keyboard.release('w')
-                        print("down")
-
-                    elif decision == 2:
-                        keyboard.press('a')
-                        keyboard.release('d')
-                        keyboard.release('s')
-                        keyboard.release('w')
-                        print("left")
-
-                    elif decision == 3:
-                        keyboard.press('d')
-                        keyboard.release('s')
-                        keyboard.release('a')
-                        keyboard.release('w')
-
-                        print("right")
-                else:
-                    pass
-                self.pacman.update(dt)
-            if current_position != last_position:
-                last_position = current_position
-                # last_position_time = time.time()
-
-            # print(output1,output2)
-            # decision = output1.index(max(output1))
-            # print(decision)
+            valid = self.move_ai(net1, game_info, genome1)
+            # print(valid)
             game_info = self.game.loop()
             self.game.render()
             pygame.display.update()
-
-            # print(time.time())
-            # Format as string
-
-            if game_info.lives < 5:
+            if valid:
                 self.calculate_fitness(genome1, genome2, game_info)
+            elif valid == None:
+                pass
+            else:
                 break
 
     def calculate_fitness(self, genome1, genome2, game_info):
-        genome1.fitness += game_info.score + game_info.time_elapsed / 1000
-        genome2.fitness += 0
-        print(genome1.fitness)
+        genome1.fitness += game_info.score / 10 + game_info.time_elapsed / 1000
+        genome2.fitness += game_info.score / 10 + game_info.time_elapsed / 1000
+        # print(genome1.fitness)
 
-    def move_ai(self, net, game_info):
-        output1 = net.activate((game_info.pacman_pos_x.x, game_info.pacman_pos_x.y,
-                                game_info.ghosts_pos[0].x - game_info.pacman_pos_x.x,
-                                game_info.ghosts_pos[0].y - game_info.pacman_pos_x.y,
-                                game_info.ghosts_pos[1].x - game_info.pacman_pos_x.x,
-                                game_info.ghosts_pos[1].y - game_info.pacman_pos_x.y,
-                                game_info.ghosts_pos[2].x - game_info.pacman_pos_x.x,
-                                game_info.ghosts_pos[2].y - game_info.pacman_pos_x.y,
-                                game_info.ghosts_pos[3].x - game_info.pacman_pos_x.x,
-                                game_info.ghosts_pos[3].y - game_info.pacman_pos_x.y,
-                                game_info.score
-                                ))
-        decision = output1.index(max(output1))
-        if decision == 0:
-            # self.game.pacman.direction = UP
-            keyboard.press('w')
-            keyboard.release('a')
-            keyboard.release('s')
-            keyboard.release('d')
-            print("up")
+    def move_ai(self, net, game_info, genome1):
+        dt = self.clock.tick(300) / 1000.0
+        pacman_pos = (game_info.pacman_pos_x.x, game_info.pacman_pos_x.y)
+        for node_pos in self.turning_points:
+            if pacman_pos == node_pos:
 
-        elif decision == 1:
-            keyboard.press('s')
-            keyboard.release('d')
-            keyboard.release('a')
-            keyboard.release('w')
-            print("down")
+                output1 = net.activate((game_info.pacman_pos_x.x, game_info.pacman_pos_x.y,
+                                        game_info.ghosts_pos[0].x - game_info.pacman_pos_x.x,
+                                        game_info.ghosts_pos[0].y - game_info.pacman_pos_x.y,
+                                        game_info.ghosts_pos[1].x - game_info.pacman_pos_x.x,
+                                        game_info.ghosts_pos[1].y - game_info.pacman_pos_x.y,
+                                        game_info.ghosts_pos[2].x - game_info.pacman_pos_x.x,
+                                        game_info.ghosts_pos[2].y - game_info.pacman_pos_x.y,
+                                        game_info.ghosts_pos[3].x - game_info.pacman_pos_x.x,
+                                        game_info.ghosts_pos[3].y - game_info.pacman_pos_x.y,
+                                        game_info.score
+                                        ))
+                decision = output1.index(max(output1))
+                if decision == 0:
+                    # self.game.pacman.direction = UP
+                    keyboard.press('w')
+                    keyboard.release('a')
+                    keyboard.release('s')
+                    keyboard.release('d')
+                    # print("up")
 
-        elif decision == 2:
-            keyboard.press('a')
-            keyboard.release('d')
-            keyboard.release('s')
-            keyboard.release('w')
-            print("left")
+                elif decision == 1:
+                    keyboard.press('s')
+                    keyboard.release('d')
+                    keyboard.release('a')
+                    keyboard.release('w')
+                    # print("down")
 
-        elif decision == 3:
-            keyboard.press('d')
-            keyboard.release('s')
-            keyboard.release('a')
-            keyboard.release('w')
+                elif decision == 2:
+                    keyboard.press('a')
+                    keyboard.release('d')
+                    keyboard.release('s')
+                    keyboard.release('w')
+                    # print("left")
 
-            print("right")
-        pass
+                elif decision == 3:
+                    keyboard.press('d')
+                    keyboard.release('s')
+                    keyboard.release('a')
+                    keyboard.release('w')
 
+                    # print("right")
+            else:
+                pass
+        self.pacman.update(dt)
+        pacman_pos = (game_info.pacman_pos_x.x, game_info.pacman_pos_x.y)
+        # print(pacman_pos)
+        current_position = (game_info.pacman_pos_x.x, game_info.pacman_pos_x.y)
+        if game_info.lives < 5:
+            genome1.fitness -= 100
+            return False
 
-# players = [(self.genome1, net, self.left_paddle, True)]
-# for (genome, net, paddle, left) in players:
-# output = net.activate(
-# (paddle.y, abs(paddle.x - self.ball.x), self.ball.y))
-# decision = output.index(max(output))
+        elif current_position != self.last_position:
+            self.last_position = current_position
+            current_time = datetime.datetime.now()
+            time_string = current_time.strftime("%H%M%S")
+            self.last_position_time = time_string
+            # print(self.last_position_time)
 
-# def calculate_fitness(self, game_info, duration):
-# self.genome.fitness += game_info.score + duration
+            # print(last_position)
+            # last_position_time = time.time()
+        elif current_position == self.last_position:
+            current_time = datetime.datetime.now()
+            time_string = current_time.strftime("%H%M%S")
+            # print(int(time_string) - int(self.last_position_time))
+            if int(time_string) - int(self.last_position_time) > 1.5:
+                # print('idle')
+                genome1.fitness -= 10
+
+                return False
+            else:
+                return True
+
+                # self.calculate_fitness(genome1, genome2, game_info)
+                pass
 
 
 def eval_genomes(genomes, config):
@@ -262,7 +205,7 @@ def run_neat(config):
     p.add_reporter(stats)
     p.add_reporter(neat.Checkpointer(1))
 
-    winner = p.run(eval_genomes, 50)
+    winner = p.run(eval_genomes, 25)
     print(stats)
     # with open("best.pickle", "wb") as f:
     # pickle.dump(winner, f)
